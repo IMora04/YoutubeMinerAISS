74798d62602f3aa057d1f12cd91e5f9aebad0320
IMora04 <ignmorper1@alum.us.es>
2024-05-06 14:32:56 +0200

    YoutubeMiner migration

---
+++ src/test/java/aiss/youtubeMiner/YoutubeMinerApplicationTests.java
@@ -0,0 +1,13 @@
+package aiss.youtubeMiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeMinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
