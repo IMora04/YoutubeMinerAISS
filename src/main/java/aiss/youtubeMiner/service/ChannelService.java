a88b4a8262107222faea8cb938aae2a37c00c9a9
Miguel <mprajin860@gmail.com>
2024-05-10 13:11:27 +0200

    Video Snippet service

--- src/main/java/aiss/youtubeMiner/service/ChannelService.java
+++ src/main/java/aiss/youtubeMiner/service/ChannelService.java
@@ -1,11 +1,14 @@
 package aiss.youtubeMiner.service;
 
+import aiss.youtubeMiner.model.channel.Channel;
 import aiss.youtubeMiner.model.channel.ChannelSearch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 public class ChannelService {
@@ -14,12 +17,20 @@ public class ChannelService {
     RestTemplate restTemplate;
 
     ResponseEntity<ChannelSearch> response;
-    public ChannelSearch getChannels(){
+    public List<Channel> getChannels(){
         String uri = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyBJcbUFSXcmamEuytMYVsiVPvdbdQkG6i4&part=snippet&type=channel";
         response = restTemplate.exchange(uri, HttpMethod.GET,
                 null, ChannelSearch.class);
 
-        return response.getBody();
+        return response.getBody().getItems();
+    }
+
+    public Channel getChannel(String id){
+        String uri = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyBJcbUFSXcmamEuytMYVsiVPvdbdQkG6i4&part=snippet&type=channel&channelId="
+                + id;
+        response = restTemplate.exchange(uri,HttpMethod.GET,null,ChannelSearch.class);
+
+        return response.getBody().getItems().get(0);
     }
 
 }
