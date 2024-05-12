36b6efad33714f698084028b6ace44703903e73c
ALBERTO BERNARDOS <albberrui@alum.us.es>
2024-05-12 19:06:30 +0200

    pom.xml dependency of documentation added

--- src/main/java/aiss/youtubeMiner/controller/ChannelController.java
+++ src/main/java/aiss/youtubeMiner/controller/ChannelController.java
@@ -3,6 +3,12 @@ package aiss.youtubeMiner.controller;
 import aiss.youtubeMiner.VMmodel.VMChannel;
 import aiss.youtubeMiner.model.channel.Channel;
 import aiss.youtubeMiner.service.ChannelService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -11,8 +17,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+
 import java.util.List;
 
+@Tag(name = "Chanenl", description = "Channel management API")
 @RestController
 @RequestMapping("/youtubeminer/channels")
 public class ChannelController {
@@ -39,15 +47,42 @@ public class ChannelController {
     @Autowired
     RestTemplate restTemplate;
 
+    @Operation( summary = "Retrieve all channels",
+            description = "Get every channel",
+            tags = { "channels", "get" })
+    @ApiResponse(responseCode = "200", description = "Channel list",
+                    content = { @Content(schema = @Schema(implementation = Channel.class),
+                    mediaType = "application/json") })
     @GetMapping
     public List<Channel> findAll() {
         return service.getChannels();
     }
 
+    @Operation(summary = "Retrieve channel by Id",
+            description = "Get the channel specified with Id",
+            tags = { "channels", "get" })
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Get one specified channel",
+            content = { @Content(schema = @Schema(implementation = Channel.class),
+                    mediaType = "application/json") }),
+        @ApiResponse(responseCode = "404", description="Channel not found",
+                content = { @Content(schema = @Schema()) })})
+
+
     @GetMapping("/{id}")
     public Channel findOne(@PathVariable String id){
         return service.getChannel(id);
     }
+
+    @Operation(summary = "Create a new channel",
+            description = "",
+            tags = { "channels", "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Get one specified channel",
+                    content = { @Content(schema = @Schema(implementation = Channel.class),
+                            mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", description="Channel not found",
+                    content = { @Content(schema = @Schema()) })})
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
